4006e6529b4bb652b891f0aff84ba41d0b73a176
leonardo-lim <leonardolim178@gmail.com>
2024-10-25 23:45:23 +0700

    chore: first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next';
+import './globals.css';
+import { Plus_Jakarta_Sans } from 'next/font/google';
+
+const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ['latin'] });
+
+const metadata: Metadata = {
+    title: 'Creble',
+    description: 'Catalog Web App with Next.js and TypeScript'
+};
+
+interface RootLayoutProps {
+    children: React.ReactNode
+}
+
+const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
+    return (
+        <html lang="en">
+            <body className={plusJakartaSans.className}>{children}</body>
+        </html>
+    );
+};
+
+export { metadata };
+export default RootLayout;
