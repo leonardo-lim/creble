8b3220d9a391c46c2a4b82ba5a6d9f81cf961d93
leonardo-lim <leonardolim178@gmail.com>
2024-10-26 23:01:22 +0700

    feat: add product search functionality

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import type { ProductType } from '@/types/product-type';
 import { useEffect, useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import Product from './Product';
@@ -8,7 +9,18 @@ import './Products.css';
 const Products: React.FC = () => {
     const baseURL = 'https://fakestoreapi.com/products';
 
-    const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState<ProductType[]>([]);
+    const [filteredProducts, setFilteredProducts] = useState<ProductType[]>([]);
+
+    const searchProduct = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+
+        const filtered = products.filter((product) => {
+            return product.title.toLowerCase().includes(value.toLowerCase());
+        });
+
+        setFilteredProducts(filtered);
+    };
 
     const getProducts = async () => {
         const response = await fetch(baseURL);
@@ -20,15 +32,24 @@ const Products: React.FC = () => {
         getProducts();
     }, []);
 
+    useEffect(() => {
+        setFilteredProducts(products);
+    }, [products]);
+
     return (
         <div className="products">
             <div className="search">
-                <input type="text" placeholder="Search products" className="search-input" />
+                <input
+                    type="text"
+                    placeholder="Search products"
+                    className="search-input"
+                    onChange={searchProduct}
+                />
                 <button type="button" className="search-button"><FaSearch /></button>
             </div>
             <h1 className="section-title">Our Products</h1>
             <div className="products-container">
-                {products.map((product, idx) => {
+                {filteredProducts.map((product, idx) => {
                     return (
                         <Product key={idx} product={product} />
                     );
