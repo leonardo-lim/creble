5d090fe8c871e517c4fbc08d728f0c607c5c9974
leonardo-lim <leonardolim178@gmail.com>
2024-10-26 18:13:06 +0700

    feat: add product component

---
+++ src/components/Products/Product.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { FaDollarSign, FaStar } from 'react-icons/fa';
+import './Product.css';
+
+interface ProductProps {
+    product: {
+        id: string;
+        title: string;
+        price: number;
+        description: string;
+        category: string;
+        image: string;
+        rating: {
+            rate: number;
+            count: number;
+        };
+    };
+}
+
+const Product: React.FC<ProductProps> = ({ product }) => {
+    const getTitle = () => {
+        const { title } = product;
+
+        if (title.length > 30) {
+            return `${title.substring(0, 27)}...`;
+        } else {
+            return title;
+        }
+    };
+
+    return (
+        <div key={product.id} className="card">
+            <div className="card-body">
+                <div className="poster">
+                    <img src={product.image} alt={product.title} />
+                </div>
+                <div className="title-box">
+                    <p className="title">{getTitle()}</p>
+                </div>
+                <hr />
+                <div className="info">
+                    <div className="price">
+                        <i className="price-icon"><FaDollarSign /></i>
+                        <span className="price-text">{product.price}</span>
+                    </div>
+                    <div className="rating">
+                        <i className="rating-icon"><FaStar /></i>
+                        <span className="rating-text">{product.rating.rate}</span>
+                    </div>
+                </div>
+                <div className="button-container">
+                    <button type="button" className="detail-button">See Details</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
