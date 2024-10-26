841edf9aa976da197b330f275446f070c4e71c10
leonardo-lim <leonardolim178@gmail.com>
2024-10-26 17:59:08 +0700

    feat: add navbar component

---
+++ src/components/Layout/Navbar.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import './Navbar.css';
+
+const Navbar: React.FC = () => {
+    return (
+        <div className="navbar">
+            <label className="brand">Creble</label>
+        </div>
+    );
+};
+
+export default Navbar;
