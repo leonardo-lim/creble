c6f0a418271a01f9c70e146a3c53219798f55171
leonardo-lim <leonardolim178@gmail.com>
2024-10-26 18:13:57 +0700

    feat: add products page view

---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import type { NextPage } from 'next';
+import Products from '@/components/Products/Products';
+
+const ProductsPage: NextPage = () => {
+    return <Products />;
+};
+
+export default ProductsPage;
